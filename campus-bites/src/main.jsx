67fc52103b3c767d9799bf67e854f02e1c0f81ad
Akash HG <akash@example.com>
2025-12-30 02:07:24 +0530

    Bump version to 1.0.1 and add debug log

--- campus-bites/src/main.jsx
+++ campus-bites/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+console.log('Campus Bites Version: 1.0.1 - Deployment Test');
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
