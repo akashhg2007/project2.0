4dcfbb8927b4a8b16fe713a92dfecf510b6ab4f2
Akash HG <akash@example.com>
2025-12-31 19:28:52 +0530

    feat: integrate vercel speed insights

--- campus-bites/src/main.jsx
+++ campus-bites/src/main.jsx
@@ -5,11 +5,14 @@ import './index.css'
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 console.log('Campus Bites Version: 1.0.1 - Deployment Test');
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || '785669315584-m6060cqqk9nqrrbd6asjnd9j6vslm588.apps.googleusercontent.com'}>
             <App />
+            <SpeedInsights />
         </GoogleOAuthProvider>
     </React.StrictMode>,
 )
